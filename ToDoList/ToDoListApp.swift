744a7308409b5361f4c0725795863683f271bbbd
SaishTiwari <tiwarisaish381@gmail.com>
2024-07-09 19:32:03 +0545

    first commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Saish Tiwari on 09/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
