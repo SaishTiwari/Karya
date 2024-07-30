1843f3df1938d73f27da430aaa62383e82ad5cbe
SaishTiwari <tiwarisaish381@gmail.com>
2024-07-30 19:00:24 +0545

    hi

--- ToDoList/View/SplashScreen .swift
+++ ToDoList/View/SplashScreen .swift
@@ -1,34 +1,50 @@
 import SwiftUI
 
 struct SplashScreen: View {
+    @EnvironmentObject  var listviewmodel : ListViewModel
     @State private var text = ""
     let fullText = "KARYA"
+    @State var isActive  : Bool = false
     
     var body: some View {
-        ZStack {
-            // Background
-            LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(.all)
+        if isActive{
+            HomePage()
             
-            // Animated Text
-            Text(text)
-                .font(.system(size: 75))
-                .fontWeight(.bold)
-                .foregroundColor(.yellow)
-                .multilineTextAlignment(.center)
-                .lineLimit(0)
-                .shadow(color: .black.opacity(0.9), radius: 10, x: 5, y: 5)
-                .onAppear {
-                    withAnimation(Animation.linear(duration: 1).delay(2)) {
-                        typeWriter()
+        }
+        else{
+            
+            
+            
+            ZStack {
+                // Background
+                LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .edgesIgnoringSafeArea(.all)
+                
+                // Animated Text
+                Text(text)
+                    .font(.system(size: 45))
+                    .fontWeight(.bold)
+                    .foregroundColor(.yellow)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(0)
+                    .shadow(color: .black.opacity(0.9), radius: 10, x: 5, y: 5)
+                    .onAppear {
+                        withAnimation(Animation.linear(duration: 2).delay(2)) {
+                            typeWriter()
+                        }
                     }
-                }
+                
+                    .onAppear(perform: {
+                        DispatchQueue.main.asyncAfter(deadline: .now()+2.3){ isActive = true}
+                    })
+                
+            }
         }
     }
     
     func typeWriter() {
         var index = 0
-        let timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+        let timer = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: true) { timer in
             if index < fullText.count {
                 let endIndex = fullText.index(fullText.startIndex, offsetBy: index + 1)
                 text = String(fullText[..<endIndex])
@@ -43,4 +59,5 @@ struct SplashScreen: View {
 
 #Preview {
     SplashScreen()
+        .environmentObject(ListViewModel())
 }
